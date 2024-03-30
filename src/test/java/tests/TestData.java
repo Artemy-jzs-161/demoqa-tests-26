e57414aa1a6d07880836ec41062669a5fe1920b4
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-03-30 18:41:46 +0700

    fix make oject faker private final

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -5,7 +5,7 @@ import com.github.javafaker.Faker;
 import java.util.Locale;
 
 public class TestData {
-    Faker faker = new Faker(new Locale("en-GB"));
+    private final Faker faker = new Faker(new Locale("en-GB"));
     String firstName = faker.name().firstName();
     String lastName = faker.name().lastName();
     String userEmail = faker.internet().emailAddress();
