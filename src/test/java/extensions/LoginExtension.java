0da778c092d0a0097c158acefb21205fc674d8cc
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-05-26 21:25:44 +0700

    add step allure for LoginExtension

--- src/test/java/extensions/LoginExtension.java
+++ src/test/java/extensions/LoginExtension.java
@@ -1,6 +1,7 @@
 package extensions;
 
 import api.AuthorizationApi;
+import io.qameta.allure.Step;
 import models.login.LoginRequestModel;
 import models.login.LoginResponseModel;
 import org.junit.jupiter.api.extension.*;
@@ -12,6 +13,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class LoginExtension implements BeforeEachCallback {
     @Override
+    @Step("Войдите в систему и установите файлы cookie")
     public void beforeEach(ExtensionContext context) {
         AuthorizationApi userApi = new AuthorizationApi();
         LoginRequestModel loginRequest = new LoginRequestModel(TestData.USERNAME, TestData.PASSWORD);
